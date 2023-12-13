82c1f18674990482f204655d95b3c58493366b19
MyFirstApplication <a82954531@gmail.com>
2023-12-13 16:48:36 +0800

    移动软件开发实验4

--- src/main/java/com/jnu/student/MainActivity.java
+++ src/main/java/com/jnu/student/MainActivity.java
@@ -3,20 +3,50 @@ package com.jnu.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class YourActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewHello;
+    private TextView textViewJNU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // 替换成你的布局文件名
+        setContentView(R.layout.activity_main);
 
-        // 找到 TextView 对象
-        TextView helloWorldTextView = findViewById(R.id.text_view_hello_world);
+        textViewHello = findViewById(R.id.textViewHello);
+        textViewJNU = findViewById(R.id.textViewJNU);
 
-        // 获取字符串资源并设置到 TextView 中
-        String helloAndroidString = getResources().getString(R.string.hello_android);
-        helloWorldTextView.setText(helloAndroidString);
+        Button buttonChangeText = findViewById(R.id.buttonChangeText);
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 交换两个TextView的文本
+                String tempText = textViewHello.getText().toString();
+                textViewHello.setText(textViewJNU.getText());
+                textViewJNU.setText(tempText);
+
+                // 显示Toast
+                Toast.makeText(MainActivity.this, "交换成功", Toast.LENGTH_SHORT).show();
+
+                // 显示AlertDialog
+                showAlertDialog("交换成功");
+            }
+        });
+    }
+
+    private void showAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setPositiveButton("OK", null)
+                .create()
+                .show();
     }
 }
+
