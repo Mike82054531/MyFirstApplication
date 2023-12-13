de2366bc91b3b36990b3742464dcb8c18e3ed022
MyFirstApplication <a82954531@gmail.com>
2023-12-13 16:23:59 +0800

    移动软件开发实验3

--- src/main/java/com/jnu/student/MainActivity.java
+++ src/main/java/com/jnu/student/MainActivity.java
@@ -3,12 +3,20 @@ package com.jnu.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class YourActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // 替换成你的布局文件名
+
+        // 找到 TextView 对象
+        TextView helloWorldTextView = findViewById(R.id.text_view_hello_world);
+
+        // 获取字符串资源并设置到 TextView 中
+        String helloAndroidString = getResources().getString(R.string.hello_android);
+        helloWorldTextView.setText(helloAndroidString);
     }
 }
